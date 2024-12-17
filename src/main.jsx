fbb5013e0a994929fefec122d626f999b073f981
Ibtisam-txd <muhammad.ibtisam@txend.com>
2024-12-17 11:45:44 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client"; // Use 'react-dom/client'
+import ReactDOM from "react-dom/client"; 
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./app/store";
 import App from "./App";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root")); // Use createRoot
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
